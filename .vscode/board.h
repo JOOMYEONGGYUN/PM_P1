d98bbfef71e0bb3a334430862a5411d0ff47cf3a
JOOMYEONGGYUN <106478922+JOOMYEONGGYUN@users.noreply.github.com>
2023-05-07 15:53:13 +0900

    basicfunction

--- .vscode/board.h
+++ .vscode/board.h
@@ -5,6 +5,8 @@
 using std::endl;
 using std::ofstream;
 
+static int count = 0;
+
 class Board {
     public:
         Board(int num_jobs, int width, int height, ofstream& output_stream);
@@ -18,12 +20,16 @@ class Board {
         void delete_page(int id);
         void modify_content(int id, char content);
         void modify_position(int id, int x, int y);
+        Page* page;
+        Page* page_save;
+        int array[32768];
+
 
     private:
         int num_jobs, width, height; 
         ofstream& output; 
         char* board; 
-        Page* page[];
+        
 };
 
 
@@ -86,23 +92,28 @@ void Board::print_job(int job_idx, char job_type, int id) {
 
 void Board::insert_page(int x1, int y1, int w1, int h1, int id1, char c1) {
     Page page1(x1,y1,w1,h1,id1,c1);
-    page[id1] = &page1;
+    page[id1] = page1;
     for(int i = 0; i < h1; i++){
         for(int j = 0; j < w1; j++){
-            board[x1+j+width*i] = c1;
+            board[x1 + j + width * i] = c1;
         }
     }
+    array[count] = id1; 
+    count++;
 }
 
 void Board::delete_page(int id) {
-    
+    for(int i = count; i>=0;i--){
+        page_save[array[i]] = page[array[i]];
+
+    }
 }
 
 void Board::modify_content(int id, char content) {
-   
-
+   page[id].setc(id, content);
 }
+
 void Board::modify_position(int id, int x, int y) {
-   
-    
+   page[id].setx(id, x);
+   page[id].sety(id, y);
 }
