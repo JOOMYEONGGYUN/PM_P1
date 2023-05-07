4837c076ae54c70a7135f40bdeb58523e973f607
JOOMYEONGGYUN <106478922+JOOMYEONGGYUN@users.noreply.github.com>
2023-05-07 12:17:38 +0900

    page1

--- .vscode/page.h
+++ .vscode/page.h
@@ -1,9 +1,30 @@
 class Page {
     public:
-
+        Page(int x1, int y1, int w1, int h1, int id1, char c1);
+        int getx(){return x;}
+        int gety(){return y;}
+        int getw(){return width;}
+        int geth(){return height;}
+        int getid(){return id;}
+        char getc(){return content;}
+        void setx(int x1){x=x1;}
+        void sety(int y1){y=y1;}
+        void setw(int w1){width=w1;}
+        void seth(int h1){height=h1;}
+        void setid(int id1){id=id1;}
+        void setc(char c1){content=c1;}
     private:
         int x, y; // position of the page on the board
         int width, height; // width and height of the page 
         int id; // unique id for each page
         char content; 
 };
+
+Page::Page(int x1, int y1, int w1, int h1, int id1, char c1){
+    x = x1;
+    y = y1;
+    width = w1;
+    height = h1;
+    id = id1;
+    content = c1;
+}
